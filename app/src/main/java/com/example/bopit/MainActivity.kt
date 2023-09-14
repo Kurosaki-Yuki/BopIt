765686671e8d0a23de80ee4e6b059a166b79a4a9
K <69327715+Kurosaki-Yuki@users.noreply.github.com>
2023-09-14 09:45:33 -0300

    splash no funciona

--- app/src/main/java/com/example/bopit/MainActivity.kt
+++ app/src/main/java/com/example/bopit/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.bopit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonAbout = findViewById<Button>(R.id.button)
+
+        buttonAbout.setOnClickListener{
+            val intentAbout = Intent(this, AboutActivity::class.java)
+            startActivity(intentAbout)
+        }
     }
+
+
 
 }
---
+++ app/src/main/java/com/example/bopit/SplashActivity.kt
@@ -0,0 +1,56 @@
+package com.example.bopit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.preference.PreferenceManager
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val value = sharedPreferences.getString("splash_time", "1000")
+
+        val fadeIn = AnimationUtils.loadAnimation(applicationContext, R.anim.fade)
+        val sizeUp = AnimationUtils.loadAnimation(applicationContext, R.anim.size)
+
+        val animationSet =AnimationSet(true)
+        animationSet.addAnimation(fadeIn)
+        animationSet.addAnimation(sizeUp)
+
+        val imageView = findViewById<ImageView>(R.id.testImg)
+
+        fadeIn.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation : Animation?){
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+
+            }
+        })
+        imageView.startAnimation(fadeIn)
+        val seconds = value?.toLong()
+        val delayMillis = seconds!!.toLong() // 2 seconds (adjust as needed)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: Close the splash screen activity
+        }, delayMillis)
+    }
+}
