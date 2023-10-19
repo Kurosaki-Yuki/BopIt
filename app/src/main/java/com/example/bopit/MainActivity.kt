bf2a602f65fb3be1bb59ef1fa82794a8e454e9cc
laboratorio1 <triallder@gmail.com>
2023-10-19 10:41:32 -0300

    benis music

--- app/src/main/java/com/example/bopit/MainActivity.kt
+++ app/src/main/java/com/example/bopit/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.bopit
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+
+private lateinit var mediaPlayer: MediaPlayer
+private lateinit var yippiePlayer: MediaPlayer
+private lateinit var screamPlayer: MediaPlayer
+private var plays = true
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,17 +21,57 @@ class MainActivity : AppCompatActivity() {
         val buttonAbout = findViewById<Button>(R.id.buttonA)
         val buttonConfig = findViewById<Button>(R.id.buttonC)
 
+        val buttonStop = findViewById<Button>(R.id.stopButton)
+        val buttonYippie = findViewById<Button>(R.id.yippieButton)
+        val buttonScream = findViewById<Button>(R.id.screamButton)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.benis_music)
+        mediaPlayer.start()
+        mediaPlayer.isLooping = true
+
         buttonAbout.setOnClickListener{
             val intentAbout = Intent(this, AboutActivity::class.java)
             startActivity(intentAbout)
         }
         buttonConfig.setOnClickListener{
             val intentAbout = Intent(this, SettingsActivity::class.java)
-            Log.d( "a","boton");
+            Log.d( "a","boton")
             startActivity(intentAbout)
+        }
+
+        buttonStop.setOnClickListener {
+            if(plays){
+                plays = false
+                mediaPlayer.pause()
+            }else if(!plays){
+                plays = true
+                mediaPlayer.start()
+            }
+
+        }
+
+        buttonYippie.setOnClickListener {
+            yippiePlayer = MediaPlayer.create(this, R.raw.yippee_tbh)
+            yippiePlayer.start()
+        }
+
+        buttonScream.setOnClickListener {
+            screamPlayer = MediaPlayer.create(this, R.raw.tbh_shatter)
+            screamPlayer.start()
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        if(mediaPlayer.isPlaying){
+            mediaPlayer.pause()
         }
     }
 
-
+    override fun onResume() {
+        super.onResume()
+        if(!mediaPlayer.isPlaying && plays){
+            mediaPlayer.start()
+        }
+    }
 
 }
